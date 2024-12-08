a1471d5c7443e010b845c138681ff480a1c53f70
Samyak Jain <samyaksk10@gmail.com>
2024-12-08 21:45:56 +0530

    authorisation routing done

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import colors from 'colors'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import connectDB from './config/db.js'
+import authRoutes from './routes/authRoute.js'
+
 // configure env (for storing private info)
 dotenv.config()
 
@@ -18,6 +20,9 @@ connectDB()
 app.use(express.json())
 app.use(morgan('dev'))
 
+
+// routes
+app.use('/api/v1/auth',authRoutes)
 
 // rest api
 app.get("/",(req,res)=>{
