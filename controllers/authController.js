b25f3d09ee4fe14cdff7bd031242886dd0610966
Samyak Jain <samyaksk10@gmail.com>
2024-12-11 18:09:01 +0530

    authorised middlewares

--- controllers/authController.js
+++ controllers/authController.js
@@ -114,3 +114,12 @@ export const loginController = async(req, res) => {
     });
   }
 };
+
+
+
+
+// test controller
+export const testController = (req,res)=>{
+ res.send("protected routes" )
+  
+}
