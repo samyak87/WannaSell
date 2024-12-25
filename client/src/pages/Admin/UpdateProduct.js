1b464598e8383e986ced53dffa3fb61d1dc6bdbc
Samyak Jain <samyaksk10@gmail.com>
2024-12-25 23:23:02 +0530

    done with updation and deletion of products

--- client/src/pages/Admin/UpdateProduct.js
+++ client/src/pages/Admin/UpdateProduct.js
@@ -5,6 +5,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import { Select } from "antd";
 import { useNavigate ,useParams} from "react-router-dom";
+import slugify from "slugify";
 const { Option } = Select;
 
 const UpdateProduct = () => {
@@ -18,23 +19,29 @@ const UpdateProduct = () => {
   const [quantity, setQuantity] = useState("");
   const [shipping, setShipping] = useState("");
   const [photo, setPhoto] = useState("");
+  const [id, setId] = useState("");
 
 
   // get single product
   const getSingleProduct = async() =>{
        try {
-        const {data} = await axios.get(`${process.env.REACT_APP_API}
-            /api/v1/product/get-product/${params.slug}`)
-          
-            setName(data.product.name);
-
+        const {data} = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/get-product/${params.slug}`)                  
+        setName(data.product.name);
+        setDescription(data.product.description);
+        setId(data.product._id);
+        setDescription(data.product.description);
+        setPrice(data.product.price);
+        setQuantity(data.product.quantity);
+        setShipping(data.product.shipping);
+        setCategory(data.product.category._id);
        } catch (error) {
         console.log(error);
-        
        }
   }
 
-  useEffect(()=>{getSingleProduct()},[])
+  useEffect(()=>{
+      getSingleProduct();
+    },[])
 
 
   // get all categories
@@ -57,8 +64,8 @@ const UpdateProduct = () => {
     getAllCategory();
   }, []);
 
-  // handling creation of product
-  const handleCreate = async (e) => {
+  // handling updation of product
+  const handleUpdate = async (e) => {
     e.preventDefault();
 
     try {
@@ -67,19 +74,16 @@ const UpdateProduct = () => {
       productData.append("description", description);
       productData.append("price", price);
       productData.append("quantity", quantity);
-      productData.append("photo", photo);
+      photo && productData.append("photo", photo);
       productData.append("category", category);
 
-      const { data } = await axios.post(
-        `${process.env.REACT_APP_API}/api/v1/product/create-product`,
-        productData
-      );
-
+      const { data } = await axios.put(`${process.env.REACT_APP_API}/api/v1/product/update-product/${id}`,productData);
+      
       if (data?.success) {
-        toast.success("Product created successfully");
-        navigate("/dashboard/admin/products");
+        toast.success("Product updated successfully");
+        navigate('/dashboard/admin/products');
       } else {
-        toast.error("Error in creating product");
+        toast.error("Error in updating product");
       }
     } catch (error) {
       console.log(error);
@@ -87,6 +91,22 @@ const UpdateProduct = () => {
     }
   };
 
+  // handling deleting of product
+   //delete a product
+   const handleDelete = async () => {
+    try {
+      let answer = window.confirm("Are You Sure want to delete this product ? ");
+      // if (!answer) return;
+      const { data } = await axios.delete(`
+        ${process.env.REACT_APP_API}/api/v1/product/delete-product/${id}`
+      );
+      toast.success("Product DEleted Succfully");
+      navigate("/dashboard/admin/products");
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
   return (
     <Layout>
       <div className="container-fluid m-3 p-3">
@@ -107,6 +127,7 @@ const UpdateProduct = () => {
                   onChange={(value) => {
                     setCategory(value);
                   }}
+                  value={category}
                 >
                   {categories?.map((c) => (
                     <Option key={c._id} value={c._id}>
@@ -117,17 +138,18 @@ const UpdateProduct = () => {
 
                 <div className="mb-3">
                   <label className="btn btn-outline-primary col-md-12 mb-3">
-                    {photo ? photo.name : "Upload photo"}
+                    {photo ? photo.name : "Change Photo"}
                     <input
                       type="file"
                       name="photo"
+                      value={photo}
                       accept="image/*"
                       onChange={(e) => setPhoto(e.target.files[0])}
                       hidden
                     ></input>
                   </label>
                   <div className="mb-3">
-                    {photo && (
+                    {photo ? (
                       <div className="text-center ">
                         <img
                           src={URL.createObjectURL(photo)}
@@ -136,12 +158,20 @@ const UpdateProduct = () => {
                           className="img img-responsive"
                         ></img>
                       </div>
-                    )}
+                    ) :  <div className="text-center ">
+                    <img
+                      src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${id}`}
+                      alt="product-photo"
+                      height={"200px"}
+                      className="img img-responsive"
+                    ></img>
+                  </div>}
                   </div>
                   <div className="mb-3">
                     <input
                       type="text"
                       required
+                      value={name}
                       placeholder="Enter name"
                       className="form-control"
                       onChange={(e) => setName(e.target.value)}
@@ -151,6 +181,7 @@ const UpdateProduct = () => {
                     <input
                       type="text"
                       required
+                      value={description}
                       placeholder="Enter description"
                       className="form-control"
                       onChange={(e) => setDescription(e.target.value)}
@@ -160,6 +191,7 @@ const UpdateProduct = () => {
                     <input
                       type="number"
                       required
+                      value={price}
                       placeholder="Enter price"
                       className="form-control"
                       onChange={(e) => setPrice(e.target.value)}
@@ -169,6 +201,7 @@ const UpdateProduct = () => {
                     <input
                       type="number"
                       required
+                      value={quantity}
                       placeholder="Enter quantity"
                       className="form-control"
                       onChange={(e) => setQuantity(e.target.value)}
@@ -184,14 +217,18 @@ const UpdateProduct = () => {
                       onChange={(value) => {
                         setShipping(value);
                       }}
+                      value={shipping ? "Yes" : "No"}
                     >
                       <Option value="0">No</Option>
                       <Option value="1">Yes</Option>
                     </Select>
                   </div>
                   <div className="mb-3">
-                    <button className="btn btn-primary" onClick={handleCreate}>
+                    <button className="btn btn-primary m-2" onClick={handleUpdate}>
                       UPDATE PRODUCT
+                    </button>
+                    <button className="btn btn-primary m-2" onClick={handleDelete}>
+                      DELETE PRODUCT
                     </button>
                   </div>
                 </div>
--- controllers/productController.js
+++ controllers/productController.js
@@ -141,51 +141,50 @@ export const deleteProductController = async(req,res) =>{
 }
 
 
-export const updateProductController = async(req,res) =>{
-    try {
-        const { name, slug, description, price, category, shipping, quantity } =
-          req.fields;
-        const { photo } = req.files;
-    
-        switch (1) {
-          case !name:
-            return res.status(500).send({error : "Name is required"})
-          case !description:
-            return res.status(500).send({error : "Description is required"})
-          case !price:
-            return res.status(500).send({error : "Price is required"})
-          case !category:
-            return res.status(500).send({error : "Category is required"})
-          case !shipping:
-            return res.status(500).send({error : "Shipping is required"})
-          case !quantity:
-            return res.status(500).send({error : "Quantity is required"})
-          case photo && photo.size>100000:
-            return res.status(500).send({error : "Photo is required and should be less than 1MB"})
-        }
-    
-        const product = await productModel.findByIdAndUpdate(req.params.pid,{
-            ...req.fields,slug:slugify(name)},{new:true})
-    
-        if(product)
-        {
-            product.photo.data = fs.readFileSync(photo.path);
-            product.photo.contentType = photo.type;
-    
-            await product.save();
-    
-            res.status(201).send({
-                message: "Product updated successfully",
-                success:true,
-                product
-            })
-        }
-      } catch (error) {
-        console.log(error);
-        res.status(500).send({
-          message: "Error in updating product",
-          success: false,
-          error,
-        });
-      }
-}
+export const updateProductController = async (req, res) => {
+  try {
+    const { name, description, price, category, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
+    //alidation
+    switch (true) {
+      case !name:
+        return res.status(500).send({ error: "Name is Required" });
+      case !description:
+        return res.status(500).send({ error: "Description is Required" });
+      case !price:
+        return res.status(500).send({ error: "Price is Required" });
+      case !category:
+        return res.status(500).send({ error: "Category is Required" });
+      case !quantity:
+        return res.status(500).send({ error: "Quantity is Required" });
+      case photo && photo.size > 1000000:
+        return res
+          .status(500)
+          .send({ error: "photo is Required and should be less then 1mb" });
+    }
+
+    const products = await productModel.findByIdAndUpdate(
+      req.params.pid,
+      { ...req.fields, slug: slugify(name) },
+      { new: true }
+    );
+    if (photo) {
+      products.photo.data = fs.readFileSync(photo.path);
+      products.photo.contentType = photo.type;
+    }
+    await products.save();
+    res.status(201).send({
+      success: true,
+      message: "Product Updated Successfully",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in Updte product",
+    });
+  }
+};
